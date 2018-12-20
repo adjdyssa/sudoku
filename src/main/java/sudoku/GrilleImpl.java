34219240f319e7261b783be005799efddfdd9d27
adjdyssa <42852204+adjdyssa@users.noreply.github.com>
2018-12-20 03:12:50 +0000

    Update GrilleImpl.java

--- src/main/java/sudoku/GrilleImpl.java
+++ src/main/java/sudoku/GrilleImpl.java
@@ -1,115 +1,172 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import java.util.Arrays;
 
 /**
  *
  * @author 128bits
  */
-public class GrilleImplTest {
-    
-    public GrilleImplTest() {
-    }
-    
-   
+public class GrilleImpl implements Grille {
 
     /**
-     * Test of main method, of class GrilleImpl.
+     * Caractere autoriser.
      */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        GrilleImpl.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    private char[] caracteresautoriser;
+    /**
+     * Caractere de case vide.
+     */
+    static final char EMPTY = '@';
+    /**
+     * Caractere possible a mettre dans la grille.
+     *
+     * pour une grille 9x9 : 1..9 jkhklhdd ld jdljdf lfklsfjdls
+     *
+     * pour une grille 16x16: 0..9-a.. f
+     */
+    static final char[] POSSIBLE = new char[]{'1', '2', '3', '4', '5', '6',
+        '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'};
+    /**
+     * grille a solutionner.
+     */
+    private static final  char[][] GRID_TO_SOLVE = {
+        {'9', '0', '0', '1', '0', '0', '0', '0', '5'},
+        {'0', '0', '5', '0', '9', '0', '2', '0', '1'},
+        {'8', '0', '0', '0', '4', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '8', '0', '0', '0', '0'},
+        {'0', '0', '0', '7', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '2', '6', '0', '0', '9'},
+        {'2', '0', '0', '3', '0', '0', '0', '0', '6'},
+        {'0', '0', '0', '2', '0', '0', '9', '0', '0'},
+        {'0', '0', '1', '9', '0', '4', '5', '7', '0'}, };
+    /**
+     * board  grille de travail.
+     */
+    private char[][] board;
 
     /**
-     * Test of getDimension method, of class GrilleImpl.
+     * fonction grille imple.
+     *
+     * @param grille initialisation grille
      */
-    @Test
-    public void testGetDimension() {
-        System.out.println("getDimension");
-        GrilleImpl instance = null;
-        int expResult = 0;
-        int result = instance.getDimension();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setValue method, of class GrilleImpl.
-     */
-    @Test
-    public void testSetValue() {
-        System.out.println("setValue");
-        int x = 0;
-        int y = 0;
-        char value = ' ';
-        GrilleImpl instance = null;
-        instance.setValue(x, y, value);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getValue method, of class GrilleImpl.
-     */
-    @Test
-    public void testGetValue() {
-        System.out.println("getValue");
-        int x = 0;
-        int y = 0;
-        GrilleImpl instance = null;
-        char expResult = ' ';
-        char result = instance.getValue(x, y);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public GrilleImpl(final char[][] grille) {
+        this.board = grille;
     }
 
     /**
-     * Test of complete method, of class GrilleImpl.
+     * fonction principal.
+     *
+     * @param args info arguments
      */
-    @Test
-    public void testComplete() {
-        System.out.println("complete");
-        GrilleImpl instance = null;
-        boolean expResult = false;
-        boolean result = instance.complete();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public static void main(final String[] args) {
+        // programme principal
     }
 
-  
     /**
-     * Test of possible method, of class GrilleImpl.
+     * fonction get dimenssion.
+     *
+     * @return
      */
-    @Test
-    public void testPossible() {
-        System.out.println("possible");
-        int x = 0;
-        int y = 0;
-        char value = ' ';
-        GrilleImpl instance = null;
-        boolean expResult = false;
-        boolean result = instance.POSSIBLEc(x, y, value);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @Override
+    public final int getDimension() {
+
+        return this.board.length;
     }
-    
+
+    /**
+     * fonction setvalue permet de definir une valeur.
+     *
+     * @param x
+     * @param y
+     * @param value
+     * @throws IllegalArgumentException
+     */
+    @Override
+public final void setValue(final int x, final int y, final char value)
+        throws IllegalArgumentException {
+
+        try {
+if ((0 < x) && (x < this.getDimension())
+        && (0 < y) && (y < this.getDimension())) {
+                if (possible(x, y, value)) {
+                    this.board[x][y] = value;
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("indices ou valeur incorrectes");
+        }
+    }
+
+    /**
+     * fonction getvalue.
+     *
+     * @param x
+     * @param y
+     * @return
+     * @throws IllegalArgumentException
+     */
+    @Override
+public final char getValue(final int x, final int y)
+        throws IllegalArgumentException {
+if ((0 < x) && (x < this.getDimension())
+&& (0 < y) && (y < this.getDimension())) {
+            return this.board[x][y];
+        } else {
+throw new IllegalArgumentException("indices incorrectes ou innexistant");
+        }
+    }
+
+    /**
+     * fonction test complet.
+     *
+     * @return
+     */
+    @Override
+    public final boolean complete() {
+        return !Arrays.stream(this.board).anyMatch("@"::equals);
+    }
+
+    /**
+     * fontion possible.
+     *
+     * @param x
+     * @param y
+     * @param value
+     * @return
+     * @throws IllegalArgumentException
+     */
+    @Override
+public final boolean possible(final int x, final int y, final char value)
+throws IllegalArgumentException {
+        caracteresautoriser = Arrays.copyOfRange(POSSIBLE, 0, getDimension());
+
+        int verif = 0;
+
+        for (int k = 0; k < caracteresautoriser.length; k++) {
+            if (caracteresautoriser[k] == value) {
+                // do something
+                verif = 1;
+                break;
+            }
+        }
+        if (verif == 0) {
+
+            return false;
+        }
+
+        for (int i = 0; i < this.getDimension(); i++) {
+            if (this.board[x][i] == value) {
+                return false;
+            }
+        }
+        for (int j = 0; j < this.getDimension(); j++) {
+
+            if (board[y][j] == value) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
 }
+
